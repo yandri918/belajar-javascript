746886ffc3b8e96215a2dcf306f562a0abead19d
yandri918 <150146344+yandri918@users.noreply.github.com>
2024-02-22 20:23:46 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const role = prompt("masukan role admin");
+
+if (role === "admin" || role === "spv") {
+    console.log("boleh mengaksesnya");
+} else {
+    console.log("akses ditolak");
+}
+const balonku = prompt("input warna balon");
+switch(balonku) {
+    case "merah" : 
+    console.log("balon saya");
+    break;
+    case "hijau" :
+        console.log("dor") ;
+        break;
+    default:
+        console.log("bukan balon saya");
+
+}
